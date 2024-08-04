8c5e538528a21e14825bf7d67f2a025ce0686aed
ChetanSawant <chetansawant.dev@gmail.com>
2024-08-04 14:42:29 +0530

    Initial commit

---
+++ src/main/java/com/plantBiz/PlantBizMainApplication.java
@@ -0,0 +1,13 @@
+package com.plantBiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantBizMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantBizMainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/plantBiz/PlantBizMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plantBiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantBizMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
